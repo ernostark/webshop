fd892071727a6186e12d6c15dfa3d1b319f9f8b0
Stark Ernő <stark.erno@gmail.com>
2024-11-25 18:52:59 +0100

    Admin panel kezdete

---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <form>
+        <div>
+            <p>Email cím</p>
+            <input type="email" name="email@email.hu" id="" required /> //7:54
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login
